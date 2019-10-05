9d7b354147bd943c5c3bfa8ba3f183cfb5bd343d
Meet Patel <patelmeet59@gmail.com>
2019-10-06 01:50:59 +0530

    The BIGGEST Commit in the history of The RedCell Foundation
    Bas...kar hi diya...
    
    @hmtj99 feelin' sleepy rn...

--- hackout/hackout/urls.py
+++ hackout/hackout/urls.py
@@ -4,11 +4,11 @@ from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('donors/logout',
+         views.LogoutView.as_view(next_page='hospitals:index'), name="donor_logout"),
     path('', include("hospitals.urls", namespace="hospitals")),
     path('donors/', include("donors.urls", namespace="donors"),),
     path('donors/login/', views.LoginView.as_view(template_name="donors/login.html",
                                                   redirect_field_name='hospital:donor-detail pk=donor.pk'), name="donor_login"),
-    path('donors/logout',
-         views.LogoutView.as_view(next_page='hospitals:index'), name="donor_logout")
 
 ]
